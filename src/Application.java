3791ddad4f14fbb6967c252854720d5ce4cdf4dd
Ali-Njf <alinjf1379@gmail.com>
2022-05-16 03:00:02 +0430

    Reverse Array

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello");
-
-        for (int i = 0; i < 10; i++){
-            System.out.println("Hello world" + i + 1);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your array size: ");
+        int arrSize = sc.nextInt();
+        int[] array1 = new int[arrSize];
+        int[] array2 = new int[arrSize];
+        for (int i = 0 ; i <= arrSize - 1 ; i++) {
+            System.out.println("Plz enter number of index " + (i + 1) + ": ");
+            array1[i] = sc.nextInt();
         }
-
-        boolean flag = false;
-        if (flag)
-            System.out.println(flag);
-        else {
-            System.out.println(flag);
+        int i =0;
+        for (int j = arrSize - 1 ; j >= 0 ; j--) {
+            array2[i] = array1[j];
+            i++;
         }
-    }
-}
+        System.out.print(Arrays.toString(array2));
+        }
+        }
